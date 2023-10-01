7d638881f5cff9b462f557a5530bd02d37b60c97
Coss <cosette.guevara@ucb.edu.bo>
2023-09-30 23:08:15 -0400

    Armado inicial con ApiPerros

--- src/main/java/ucb/edu/ejercicioApi/api/TestApi.java
+++ src/main/java/ucb/edu/ejercicioApi/api/TestApi.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ucb.edu.ejercicioApi.entity.News;
+import ucb.edu.ejercicioApi.entity.DogEntity;
 
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestApi {
     private static Logger logger = LoggerFactory.getLogger(TestApi.class);
     public class MediaStackApiInvoker {
-        public static ResponseEntity<News> invokeApi(String apiUrl, String apiKey) {
+        public static ResponseEntity<DogEntity> invokeApi(String apiUrl, String apiKey) {
             RestTemplate restTemplate = new RestTemplate();
 
             // Agregar la clave de acceso (API key) como query parameter
             apiUrl = apiUrl + "?access_key=" + apiKey;
 
-            ResponseEntity<News> response = restTemplate.getForEntity(apiUrl, News.class);
+            ResponseEntity<DogEntity> response = restTemplate.getForEntity(apiUrl, DogEntity.class);
             return response;
         }
 
@@ -31,10 +31,10 @@ public class TestApi {
             String apiUrl = "http://api.mediastack.com/v1/news";
             String apiKey = "3513229c530addc65459218af3a3abab";
 
-            ResponseEntity<News> response = invokeApi(apiUrl, apiKey);
+            ResponseEntity<DogEntity> response = invokeApi(apiUrl, apiKey);
 
             if (response.getStatusCode().is2xxSuccessful()) {
-                News news = response.getBody();
+                DogEntity news = response.getBody();
                 System.out.println("Respuesta de la API: " + news);
             } else {
                 System.out.println("Error en la solicitud. Código de respuesta: " + response.getStatusCodeValue());
